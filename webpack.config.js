4173ebab54d93ae9fd031179ac240e2b1cbba18c
Fernando Javier Averanga Aruquipa <nandes.ingsistemas@gmail.com>
2024-11-15 14:38:08 -0400

    IMP: improvement in the execution of each load test

--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,49 @@ const path = require('path');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
 const GlobEntries = require('webpack-glob-entries');
+const fs = require('fs');
+
+class GenerateShellScriptsPlugin {
+  apply(compiler) {
+    compiler.hooks.emit.tapAsync('GenerateShellScriptsPlugin', (compilation, callback) => {
+      const outputPath = compilation.outputOptions.path;
+      if (!fs.existsSync(outputPath)) {
+        fs.mkdirSync(outputPath, { recursive: true });
+      }
+
+      Object.keys(compilation.assets).forEach((filename) => {
+        const baseName = filename.replace(/\.js$/, '');
+        const extension = filename.split('.').pop();
+        if (extension === 'js') {
+          const shellScriptPath = path.join(compilation.outputOptions.path, `${baseName}.sh`);
+          const shellScriptContent = `#!/bin/bash
+docker compose up -d influxdb grafana
+echo "-----------------------------------------------------------------------------------------"
+echo "Pruebas de carga con Grafana dashboard http://localhost:3000/d/k6/k6-load-testing-results"
+echo "-----------------------------------------------------------------------------------------"
+docker compose run --rm k6 run /scripts/${filename}
+`;
+          const powershellScriptPath = path.join(compilation.outputOptions.path, `${baseName}.ps1`);
+          const powershellScriptContent = `docker compose up -d influxdb grafana
+Write-Output "-----------------------------------------------------------------------------------------"
+Write-Output "Pruebas de carga con Grafana dashboard http://localhost:3000/d/k6/k6-load-testing-results"
+Write-Output "-----------------------------------------------------------------------------------------"
+docker compose run --rm k6 run /scripts/${filename}
+`;
+          fs.writeFileSync(shellScriptPath, shellScriptContent, 'utf8');
+          fs.chmodSync(shellScriptPath, '755');
+          fs.writeFileSync(powershellScriptPath, powershellScriptContent, 'utf8');
+        }
+      });
+
+      callback();
+    });
+  }
+}
 
 module.exports = {
   mode: 'production',
-  entry: GlobEntries('./src/*test*.ts'), // Generates multiple entry for each test
+  entry: GlobEntries('./src/*test*.ts'),
   output: {
     path: path.join(__dirname, 'scripts'),
     libraryTarget: 'commonjs',
@@ -26,23 +65,21 @@ module.exports = {
   target: 'web',
   externals: /^(k6|https?\:\/\/)(\/.*)?/,
   // Generate map files for compiled scripts
-  devtool: "source-map",
+  // devtool: "source-map",
   stats: {
     colors: true,
   },
   plugins: [
     new CleanWebpackPlugin(),
-    // Copy assets to the destination folder
-    // see `src/post-file-test.ts` for an test example using an asset
     new CopyPlugin({
       patterns: [{ 
         from: path.resolve(__dirname, 'assets'), 
         noErrorOnMissing: true 
       }],
     }),
+    new GenerateShellScriptsPlugin(),
   ],
   optimization: {
-    // Don't minimize, as it's not used in the browser
     minimize: false,
   },
 };
